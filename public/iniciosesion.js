7cb50f013167f2c5cbd36137eaac3ff1f21f4a06
Jan Nogueira <jannogueiraacosta@gmail.com>
2025-04-09 23:25:35 +0200

    register js

--- public/iniciosesion.js
+++ public/iniciosesion.js
@@ -21,11 +21,48 @@ $(document).ready(function () {
                 },
                 success: function (response) {
                     alert('Inicio de sesión completado.');
-                    // Aquí puedes redirigir o guardar token si lo necesitas
                     console.log(response);
                 },
                 error: function () {
                     alert('Error en el inicio de sesión, comprueba los datos.');
+                }
+            });
+        }
+    });
+    $('#btnRegistro').click(function () {
+        var usuario = $('#rusuario').val().trim();
+        var correo = $('#remail').val().trim();
+        var password = $('#rpassword').val().trim();
+
+        if (usuario === '') {
+            alert('No has rellenado el campo de usuario');
+        } else if (password === '') {
+            alert('No has rellenado el campo de contraseña');
+        }else if (correo === '') {
+            alert('No has rellenado el campo de contraseña');
+        } else {
+            $.ajax({
+                url: 'http://localhost:8080/TocaBolas/users/register',
+                method: 'POST',
+                data: {
+                    username: usuario,
+                    correo: correo,
+                    password: password
+                },
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                success: function (response) {
+                    alert('Registro completado.');
+                    console.log(response);
+                },
+                error: function (xhr) {
+                    if (xhr.status === 409) {
+                        alert('El usuario ya existe.');
+                    } else {
+                        alert('Error en el registro, comprueba los datos.');
+                    }
                 }
             });
         }
