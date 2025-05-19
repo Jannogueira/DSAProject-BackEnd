725ba1d8e7d0444d30c97932dcbf939e9fc6cc5c
Jan Nogueira <jannogueiraacosta@gmail.com>
2025-05-19 14:17:17 +0200

    probando parches para el svr

--- public/scripts/shop.js
+++ public/scripts/shop.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
         success: function (productos) {
             const contenedor = $('#productos');
             contenedor.empty();
-
             productos.forEach(({ id, nombre, descripcion, precio, url_icon }) => {
                 const card = `
                     <div class="col-md-4 mb-4">
@@ -15,7 +14,6 @@ $(document).ready(function () {
                                 <h5 class="card-title">${nombre}</h5>
                                 <p class="card-text">${descripcion}</p>
                                 <p class="card-text fw-bold text-success">${precio} monedas</p>
-                                
                             </div>
                             <button class="btn btn-primary w-75 mt-auto mb-2             mx-auto" data-id="${id}">Agregar al carrito</button>
                         </div>
